33c84f78de8d5ea21f9543811c342e6e242800f3
FilippovaLena <lenchf96@gmail.com>
2024-12-23 01:31:06 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,49 @@
+let energy = 10;
+
+console.log(`Текущий уровень энергии: ${energy}`)
+
+while (energy >= 0) {
+    console.log(energy)
+
+    // energy = energy - 1
+    // energy -= 1
+    energy--
+}
+
+console.log("Программа завершена. Робот отдыхает :)")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// console.log("Hello JS!");
