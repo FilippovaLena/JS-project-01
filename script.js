c49fe122cd87599c57e0c1aa68a4679fc02115c9
FilippovaLena <lenchf96@gmail.com>
2024-12-26 13:33:04 +0300

    Константы оформила в PascalCase.

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-let energy = 10;
-
 console.log(`Текущий уровень энергии: ${energy}`)
+
+const MIN_ENERGY = 0
+const ENERGY_LIMIT = 3
+const FULL_ENERGY = 10
+
+let energy = FULL_ENERGY;
 
 while (energy >= 0) {
     console.log(energy)
-
+    if (energy === MIN_ENERGY) {
+        console.log("Робот грустит... :,( Нужно подзарядить!")
+        break
+    } else if (energy <= ENERGY_LIMIT) {
+        console.log("Робот начинает чувствовать усталость... :О")
+    } else {
+        console.log("Робот счастлив и бодрствует! ;)")
+        
+    }
     // energy = energy - 1
     // energy -= 1
     energy--
